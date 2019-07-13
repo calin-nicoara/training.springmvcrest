50e2b9025527a2339f8451503c48f82fe9289552
Calin Nicoara <calin.nicoara@esolutions.ro>
2019-07-13 09:31:14 +0300

    First commit.

---
+++ src/main/java/ro/cni/training/springmvcrest/SpringmvcrestApplication.java
@@ -0,0 +1,13 @@
+package ro.cni.training.springmvcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcrestApplication.class, args);
+    }
+
+}
