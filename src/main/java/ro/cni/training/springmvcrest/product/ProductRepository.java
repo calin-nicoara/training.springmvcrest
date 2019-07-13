47ff49970ee88651e6369654a35bd6758da01e81
Calin Nicoara <calin.nicoara@esolutions.ro>
2019-07-13 18:06:01 +0300

    Added product.

---
+++ src/main/java/ro/cni/training/springmvcrest/product/ProductRepository.java
@@ -0,0 +1,6 @@
+package ro.cni.training.springmvcrest.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
