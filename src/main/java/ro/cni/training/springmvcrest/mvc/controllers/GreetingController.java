6b0658e097145ecdb5b74485aecd8bf8362b3a32
Calin Nicoara <calin.nicoara@esolutions.ro>
2019-07-13 14:58:05 +0300

    Added extra redirect

--- src/main/java/ro/cni/training/springmvcrest/mvc/controllers/GreetingController.java
+++ src/main/java/ro/cni/training/springmvcrest/mvc/controllers/GreetingController.java
@@ -5,13 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.TimeZone;
+
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           TimeZone timezone,
                            Model model) {
+        System.out.println(timezone);
         model.addAttribute("name", name);
         return "greeting";
     }
+
+    @GetMapping("/redirect")
+    public String redirect() {
+        return "redirect:greeting";
+
+        // Can also use relative or absolute paths
+//        return "redirect:/greeting";
+//        return "redirect:http://localhost:8199/greeting";
+    }
+
+
 }
